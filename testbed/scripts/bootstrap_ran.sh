429abf080c53513b744741965bf5ba35d0ca2754
vetletm <vetlemoen@gmail.com>
2021-03-12 16:28:20 +0100

    add updated config-files and scripts for RAN VM

--- testbed/scripts/bootstrap_ran.sh
+++ testbed/scripts/bootstrap_ran.sh
@@ -3,7 +3,9 @@
 set +x
 
 apt-get update
-apt-get install -y git
+echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y git tshark linux-image-5.4.0-66-lowlatency linux-headers-5.4.0-66-lowlatency
 
 useradd -m -d /home/netmon -s /bin/bash netmon
 echo "netmon:netmon" | chpasswd
@@ -12,6 +14,13 @@ chmod 440 /etc/sudoers.d/99_netmon
 usermod -aG vboxsf netmon
 
 mkdir /home/netmon/src
-chown -R netmon:netmon /home/netmon/src
 
-ip route add 192.168.61.0/24 via 10.10.1.2
+BASE_DIR="/home/netmon/src"
+cd "$BASE_DIR"
+git clone https://gitlab.eurecom.fr/oai/openairinterface5g.git enb_folder
+cd enb_folder
+git checkout v1.2.2
+cd ..
+cp -R enb_folder/ ue_folder
+
+chown -R netmon:netmon /home/netmon/
