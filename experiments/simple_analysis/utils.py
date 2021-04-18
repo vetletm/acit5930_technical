049edf5902e019b74a1056d6d98099235e8c6d7f
vetletm <vetlemoen@gmail.com>
2021-04-18 11:42:36 +0200

    add more methods to utils

--- experiments/simple_analysis/utils.py
+++ experiments/simple_analysis/utils.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 matplotlib.use('svg')
 
 
-def save_fig(data, fig_labels, title, ylabel, fig_name):
+def save_boxplot(data, fig_labels, title, ylabel, fig_name):
     """
     Draws and saves figure with given data, labels, title, and given figure name
     :param data: Dataframe to draw
@@ -20,6 +20,46 @@ def save_fig(data, fig_labels, title, ylabel, fig_name):
     plt.title(title)
     plt.grid()
     plt.ylabel(ylabel)
+    plt.savefig(fig_name)
+    plt.close(fig)
+
+
+def save_scatterplot(x, y, title, ylabel, xlabel, fig_name):
+    """
+    Draws and saves a scatterplot with given parameters
+    :param x: Data to plot in x
+    :param y: Data to plot in y
+    :param title: Title of the figure
+    :param ylabel: Y label
+    :param xlabel: X label
+    :param fig_name: Name to save as
+    :return: None
+    """
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(x, y)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.savefig(fig_name)
+    plt.close(fig)
+
+
+def save_histogram(x, y, bins, title, xlabel, fig_name):
+    """
+    Draws two histograms side by side
+    :param x: Data for first
+    :param y: Data for second
+    :param bins: N bins
+    :param title: Title of the histograms
+    :param xlabel: Label of X
+    :param fig_name: Name to save figure as on disk
+    :return: None
+    """
+    fig, axs = plt.subplots(1, 2, sharey=True, sharex=True, tight_layout=True)
+    axs[0].hist(x, bins=bins)
+    axs[1].hist(y, bins=bins)
+    plt.title(title)
+    plt.xlabel(xlabel)
     plt.savefig(fig_name)
     plt.close(fig)
 
