ac8b572717beb4dc6903cebc9fac4f84424a7ccb
vetletm <vetlemoen@gmail.com>
2021-04-23 11:57:20 +0200

    add line regression to scatter plots

--- experiments/simple_analysis/plot_timediffs.py
+++ experiments/simple_analysis/plot_timediffs.py
@@ -3,7 +3,8 @@ import numpy as np
 
 from datetime import datetime
 
-from utils import save_scatterplot, save_histogram, save_boxplot
+from utils import save_scatter_with_line_regr, save_histogram
+
 
 print(datetime.now(), 'Starting parsing')
 # These are more than 580,000 lines each, be patient
@@ -15,8 +16,8 @@ s2_timediffs_d = pd.read_json('data/with_delay/int/s2_timediffs.json').sample(50
 s3_timediffs_d = pd.read_json('data/with_delay/int/s3_timediffs.json').sample(5000)
 print(datetime.now(), 'Parsed INT with delay, 10000 samples')
 
-s2_timediffs_ed = pd.read_json('data/with_delay/int/s2_timediffs.json').sample(5000)
-s3_timediffs_ed = pd.read_json('data/with_delay/int/s3_timediffs.json').sample(5000)
+s2_timediffs_ed = pd.read_json('data/with_delay/int_epc_delay/s2_timediffs.json').sample(5000)
+s3_timediffs_ed = pd.read_json('data/with_delay/int_epc_delay/s3_timediffs.json').sample(5000)
 print(datetime.now(), 'Parsed INT with delay at EPC, 10000 samples')
 
 items = [
@@ -36,15 +37,14 @@ for item in items:
     x = df['time_diff']
     y = df['inc_time_diff']
 
+    m, b = np.polyfit(x, y, 1)
+
     title = f'Time Diff vs Inc Time Diff, {name}'
     # Draw and save figures
-    save_histogram(x, y, 30, title, 'Millisecs', f'figures/int/{name}_td_v_itd_hist_delay')
-    # save_scatterplot(x, y, title, 'inc_time_diff', 'time_diff', f'figures/int/{name}_td_v_itd_scatter_delay')
+    # save_histogram(x, y, 30, title, 'Millisecs', f'figures/int/hists/{name}_td_v_itd_hist')
+    save_scatter_with_line_regr(x, y, m, b, title, 'inc_time_diff', 'time_diff',
+                                f'figures/int/scatter_line_reg/{name}_td_v_itd_scatter_lineregr')
 
-    print('\t', datetime.now(), f'Draw histogram and scatterplott of {name}')
-#    save_boxplot([x, y], ['time_diff', 'inc_time_diff'],
-#                 f'Time Diff vs Inc Time Diff, {name}',
-#                 'milliseconds',
-#                 f'figures/int/{name}_td_v_itd_box_delay'
-#                 )
+    print(datetime.now(), f'--- Draw histogram and scatterplott of {name}')
+
 print(datetime.now(), 'Finished parsing and drawing')
--- experiments/simple_analysis/utils.py
+++ experiments/simple_analysis/utils.py
@@ -44,6 +44,30 @@ def save_scatterplot(x, y, title, ylabel, xlabel, fig_name):
     plt.close(fig)
 
 
+def save_scatter_with_line_regr(x, y, m, b, title, ylabel, xlabel, fig_name):
+    """
+    Takes x, y and draws scatter plot with line regression
+    :param x: Data to plot in x
+    :param y: Data to plot in y
+    :param m: Slope
+    :param b: Intercept
+    :param title: Title of the figure
+    :param ylabel: Y label
+    :param xlabel: X label
+    :param fig_name: Name to save as
+    :return: None
+    """
+    fig = plt.figure(figsize=(10, 10))
+    plt.plot(x, y, 'o')
+    plt.plot(x, m*x + b)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.savefig(fig_name)
+    plt.close(fig)
+
+
 def save_histogram(x, y, bins, title, xlabel, fig_name):
     """
     Draws two histograms side by side
@@ -58,8 +82,10 @@ def save_histogram(x, y, bins, title, xlabel, fig_name):
     fig, axs = plt.subplots(1, 2, sharey=True, sharex=True, tight_layout=True)
     axs[0].hist(x, bins=bins)
     axs[1].hist(y, bins=bins)
-    plt.title(title)
-    plt.xlabel(xlabel)
+    axs[0].set_title('time_diff')
+    axs[0].set_xlabel(xlabel)
+    axs[1].set_title('inc_time_diff')
+    axs[1].set_xlabel(xlabel)
     plt.savefig(fig_name)
     plt.close(fig)
 
